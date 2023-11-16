43b9334f963cb17cc221627ab07ce1b13b8cac36
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2023-11-16 11:05:32 +0700

    first commit

---
+++ Fibonacci1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Masukkan jumlah elemen dalam rangkaian Fibonacci: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Rangkaian Fibonacci:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int Fibonacci(int n)
+         //static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Fibonacci1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int input = 5; // You can change this value based on your test case
+            int expected = 5; // You need to replace this with the expected result of Fibonacci(5)
+
+            // Act
+            int result = Program.Fibonacci(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+
+        [TestMethod]
+        public void Fibonacci_WithInput0_Returns0()
+        {
+            // Arrange
+            int input = 0;
+            int expected = 0;
+
+            // Act
+            int result = Program.Fibonacci(input);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
